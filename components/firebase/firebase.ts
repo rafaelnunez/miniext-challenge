a46c74e8c5516e6acab05854cefa3b8dd5c454ed
Rafael Nunez Manotas <rafael.nunez@ingeniousio.com>
2024-06-04 23:16:55 -0500

    message

---
+++ components/firebase/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore';
+import { /* connectStorageEmulator, */ getStorage } from 'firebase/storage';
+// import { isDev } from '../isDev';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(firebaseApp);
+export const baseBucketName = process.env.NEXT_PUBLIC_BASE_BUCKET_NAME;
+
+/* if (isDev) {
+    connectFirestoreEmulator(firestore, '127.0.0.1', 8081);
+} */
